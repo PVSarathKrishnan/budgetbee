5cdf407e15fa4f723740978790a07ee15f194f40
Sarath Krishnan <sarath7916@gmail.com>
2024-01-04 18:08:40 +0530

    final commits

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,12 @@ import 'package:budgetbee/db/models/category_model.dart';
 
 import 'package:budgetbee/presentation/screens/splash_screen/page/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Hive and setup directories
-  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDir.path);
   await Hive.initFlutter();
 
   // Register Hive adapters
